30eb3382c65668dd666fe9aa199594be90a39462
mahiawasthi23 <mahima23@navgurukul.org>
2025-05-05 09:58:59 +0530

    add css file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter-container">
+      <h1>Counter</h1>
+      <div className="counter-value">{count}</div>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Counter;
